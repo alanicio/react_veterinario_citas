9c8bd06277f275cb96f8640451fc5b8a9b134d4d
Alan <alanicio98@outlook.com>
2020-10-08 16:06:21 -0500

    Componente Cita creado, ya se muestran la citas guardads

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-const Formulario = () => {
+const Formulario = ({crearCita}) => {
 	// Crear State de citas
 	/*
 		NOTA: usar mismos nombres para names de inputs en el state
@@ -43,7 +43,19 @@ const Formulario = () => {
 
 		//Asignar ID
 		cita.id = uuidv4();
-		console.log(cita);
+		
+		// Crear la cita
+		crearCita(cita);
+
+		//Reiniciar el formulario
+		actualizarCita({
+			mascota: '',
+			propietario: '',
+			fecha: '',
+			hora: '',
+			sintomas: '',
+		});
+
 	}
 
 	return (
