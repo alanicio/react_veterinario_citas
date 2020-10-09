556a28a64f29943e508378a52bb684a5e61df7b6
Bitbyte <hectoraldairah@protonmail.com>
2020-10-08 21:28:16 -0500

    desctructuring squash

--- src/components/Cita.js
+++ src/components/Cita.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const Cita = ({cita}) => (
-
-	<div className="cita">
-		<p>Mascota: <span>{ cita.mascota }</span> </p> 
-		<p>Dueño: <span>{ cita.propietario }</span> </p> 
-		<p>Fecha: <span>{ cita.fecha }</span> </p> 
-		<p>Hora: <span>{ cita.hora }</span> </p> 
-		<p>Síntomas: <span>{ cita.sintomas }</span> </p> 
-	</div>
-
+const Cita = ({ mascota, propietario, fecha, hora, sintomas }) => (
+  <div className='cita'>
+    <p>Mascota: <span>{mascota}</span></p>
+    <p>Dueño: <span>{propietario}</span></p>
+    <p>Fecha: <span>{fecha}</span></p>
+    <p>Hora: <span>{hora}</span></p>
+    <p>Síntomas: <span>{sintomas}</span></p>
+  </div>
 );
 
 export default Cita;
