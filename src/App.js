f8aaaf78a17a84602188549423ed196c0535d988
Alan <alanicio98@outlook.com>
2020-10-09 14:23:50 -0500

    Las citas se guardan en localStorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 import Cita from './components/Cita';
 
 function App() {
+
+  // Citas en localStorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales){
+    citasIniciales = [];
+  }
+
   // Arreglo de citas
-  const [citas, guardarCitas] = useState([]);
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+  // Use effect para realizar ciertas operaciones cuando el state cambia
+   /* 
+    NOTA: useEffect SIEMPRE es un arroy function. Se ejecuta cuando el componente esta listo
+          (como document.ready) pero tambien cuando hay cambios en el componente.
+          Para decirle a useEffect que solo se ejecute una vez tienes que pasarle como segundo parametro
+          despues de la función, un arreglo vacio. Ese segundo parametro siempre es un arreglo y se
+          conoce como dependencias. Cada vez que el state pasado como dependencia cambie (en este caso citas
+          es la dependencia) se va a ejecutar el useEffect
+  */
+  useEffect( () => {
+    localStorage.setItem('citas', JSON.stringify(citas));
+  }, [citas] );
 
   // Función  que tome las citas actuales y agregue la nueva
   const crearCita = (cita) => {
@@ -16,6 +36,9 @@ function App() {
     const nuevasCitas = citas.filter(cita => cita.id != id);
     guardarCitas(nuevasCitas);
   }
+
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus Citas';
   
 
   return (
@@ -28,7 +51,7 @@ function App() {
             <Formulario crearCita={crearCita} />
           </div>
           <div className='one-half column'>
-            <h2>Administra tus citas</h2>
+            <h2>{ titulo }</h2>
             {citas.map((cita) => (
                 <Cita 
                   key={cita.id}
